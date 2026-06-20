df19b435a32080f1284f0902ca463c7692490c30
Ayka Alliance <iaykacare@gmail.com>
2026-06-20 10:57:26 +0530

    Clean: Remove Google Sheets background sync from instrumentation.ts

--- instrumentation.ts
+++ instrumentation.ts
@@ -1,15 +1,6 @@
 export async function register() {
   if (process.env.NEXT_RUNTIME === 'nodejs') {
-    try {
-      const syncModule = await import('./utils/background-sync.js');
-      const startBackgroundSync = syncModule.startBackgroundSync || (syncModule.default && syncModule.default.startBackgroundSync) || syncModule.default;
-      if (typeof startBackgroundSync === 'function') {
-        startBackgroundSync();
-      } else {
-        console.error('[Instrumentation] startBackgroundSync is not a function');
-      }
-    } catch (e) {
-      console.error('[Instrumentation] Failed to start background sync:', e);
-    }
+    console.log('[Instrumentation] CRM standalone server starting up...');
   }
 }
+
